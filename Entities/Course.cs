efdeaa91ccb13b79567c90ea00e26e445089bf71
karani-ken <academice79@gmail.com>
2023-08-29 17:37:11 +0300

    Add project files.

---
+++ Entities/Course.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JituUdemy.Entities
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+      
+        [ForeignKey("InstructorId")]
+        public Instructor Instructor { get; set; }
+
+        public Guid InstructorId { get; set; } 
+        public List<User> Users { get; set; } = new List<User>();
+
+    }
+}
